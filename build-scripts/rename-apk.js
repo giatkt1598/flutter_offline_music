cb01ef59c41830de65bf5017eb5d4942b9a9769e
giatkt1598 <59951935+giatkt1598@users.noreply.github.com>
2025-03-17 21:49:52 +0700

    1.0.3+1

---
+++ build-scripts/rename-apk.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+const getAppVersion = () => {
+    const filePath = path.join(import.meta.dirname, '..', 'pubspec.yaml');
+    const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+    const verIndex = lines.findIndex(x => x.startsWith('version: '));
+    let verNumber = lines[verIndex].split(' ')[1];
+    return verNumber.trim();
+}
+
+const apkDir = path.join(import.meta.dirname, '..', 'build/app/outputs/flutter-apk');
+const apkOldPath = path.join(apkDir, 'app-release.apk');
+const apkNewPath = path.join(apkDir, `music_player_${getAppVersion()}.apk`);
+
+if (fs.existsSync(apkOldPath)) {
+    fs.renameSync(apkOldPath, apkNewPath);
+    console.log(`Apk: ${apkNewPath}`);
+} else {
+    console.error(`Not found ${apkOldPath}`);
+}
+
+
+
---
+++ build-scripts/up-version.js
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import path from 'path';
+import cp from 'child_process';
+
+const filePath = path.join(import.meta.dirname, '..', 'pubspec.yaml');
+
+const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+
+const verIndex = lines.findIndex(x => x.startsWith('version: '));
+let verNumber = lines[verIndex].split(' ')[1];
+const verNumArr = verNumber.split('.');
+const lastNumbers = verNumArr[2].split('+');
+verNumArr.pop();
+verNumArr.push((+lastNumbers[0] + 1) + '+' + lastNumbers[1]);
+
+const newVersion = verNumArr.join('.');
+lines[verIndex] = 'version: ' + newVersion;
+
+fs.writeFileSync(filePath, lines.join('\n'), 'utf-8');
+
+cp.execSync(`git add . && git commit -m "${newVersion}"`);
+
+console.log(`Up version: ${newVersion}`);
+
+
