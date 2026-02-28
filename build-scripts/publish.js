5f44293bd414b3fbd6974bb1c0d35f2b66f33149
GiaTK <giatkt1598@gmail.com>
2026-02-28 11:41:06 +0700

    update script

---
+++ build-scripts/publish.js
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import path from 'path';
+import cp from 'child_process';
+
+const filePath = path.join(import.meta.dirname, '..', 'pubspec.yaml');
+const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+const verIndex = lines.findIndex(x => x.startsWith('version: '));
+
+if (verIndex < 0) {
+  throw new Error('Cannot find version in pubspec.yaml');
+}
+
+const version = lines[verIndex].split(' ')[1];
+const tagVersion = `v${version.split('+')[0]}`;
+
+cp.execSync('git add .', { stdio: 'inherit' });
+cp.execSync(`git commit -m "[Build APK] ${version}"`, { stdio: 'inherit' });
+
+try {
+  cp.execSync(`git rev-parse -q --verify refs/tags/${tagVersion}`, {
+    stdio: 'ignore',
+  });
+  console.log(`Tag already exists, skip tag: ${tagVersion}`);
+} catch {
+  cp.execSync(`git tag ${tagVersion}`, { stdio: 'inherit' });
+  console.log(`Created tag: ${tagVersion}`);
+}
+
+cp.execSync('git push --follow-tags', { stdio: 'inherit' });
+console.log(`Published version: ${version}`);
--- build-scripts/up-version.js
+++ build-scripts/up-version.js
@@ -1,36 +1,18 @@
 import * as fs from 'fs';
 import path from 'path';
-import cp from 'child_process';
 
 const filePath = path.join(import.meta.dirname, '..', 'pubspec.yaml');
 
 const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
 
 const verIndex = lines.findIndex(x => x.startsWith('version: '));
-const verNumber = lines[verIndex].split(' ')[1];
-const verNumArr = verNumber.split('.');
-const lastNumbers = verNumArr[2].split('+');
-verNumArr.pop();
-verNumArr.push((+lastNumbers[0] + 1) + '+' + lastNumbers[1]);
+const verNumber = lines[verIndex].split(' ')[1].split('+')[0];
+const verNumArr = verNumber.split('.').map(x => +x);
+verNumArr[2] = verNumArr[2] + 1;
 
 const newVersion = verNumArr.join('.');
 lines[verIndex] = 'version: ' + newVersion;
-const tagVersion = `v${newVersion.split('+')[0]}`;
 
 fs.writeFileSync(filePath, lines.join('\n'), 'utf-8');
 
-cp.execSync('git add .', { stdio: 'inherit' });
-cp.execSync(`git commit -m "[Build APK] ${newVersion}"`, { stdio: 'inherit' });
-
-try {
-  cp.execSync(`git rev-parse -q --verify refs/tags/${tagVersion}`, {
-    stdio: 'ignore',
-  });
-  console.log(`Tag already exists, skip tag: ${tagVersion}`);
-} catch {
-  cp.execSync(`git tag ${tagVersion}`, { stdio: 'inherit' });
-  console.log(`Created tag: ${tagVersion}`);
-}
-
 console.log(`Up version: ${newVersion}`);
-
